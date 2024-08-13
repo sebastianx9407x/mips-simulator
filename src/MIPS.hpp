259d17347fc3766ab3777cfdec26aa138aa6b4c2
sebastianx9407x <sebastianx9407x@gmail.com>
2024-08-13 14:20:02 -0400

    adding mapping of registers and instructions

---
+++ src/MIPS.hpp
@@ -0,0 +1,44 @@
+#ifndef MIPS_HPP
+#define MIPS_HPP
+
+#include "Instruction.hpp"
+#include "MIPSParser.hpp"
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <optional>
+
+class MIPS
+{
+public:
+    MIPS();
+    MIPS(const std::string &file);
+    ~MIPS();
+    // Table to map label to adress for jumping
+    std::unordered_map<std::string, uint32_t> &labelTable;
+    // Data tables
+    std::unordered_map<std::string, std::string> &dataTable;
+    // List of each instruction sequentially found in file
+    std::vector<Instruction> &instructions;
+    std::string &global;
+    static const std::unordered_map<std::string, uint32_t> registerMap;
+    static const std::unordered_map<std::string, uint32_t> instructionMap;
+
+private:
+    MIPSParser parser;
+};
+
+// Define a structure to hold the register information
+struct RegisterInfo
+{
+    uint32_t decVal;    // The integer representation
+    std::string binStr; // The binary string representation
+};
+
+struct InstructionInfo
+{
+    std::string opcode;                  // The opcode in binary string form
+    std::optional<std::string> function; // The function in binary string form (optional)
+};
+
+#endif
