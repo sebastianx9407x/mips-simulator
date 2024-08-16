f8c91443e6daebea652b94daa230c562d7fc9b83
sebastianx9407x <sebastianx9407x@gmail.com>
2024-08-15 23:46:13 -0400

    helpers for bit manipulationh

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <iostream>
 #include <cstdint>
+#include <limits>
 
 std::vector<std::string> split(const std::string &s, char delim)
 {
@@ -22,4 +23,29 @@ std::string toBinaryString(uint32_t value)
 {
     std::bitset<32> bits(value);
     return bits.to_string();
+}
+
+bool fitsIn16Bits(std::int32_t value)
+{
+    return value >= std::numeric_limits<std::int16_t>::min() && value <= std::numeric_limits<std::int16_t>::max();
+}
+
+bool fitsIn32Bits(std::int32_t value)
+{
+    return value >= std::numeric_limits<std::int32_t>::min() && value <= std::numeric_limits<std::int32_t>::max();
+}
+
+std::int32_t handleValue(const std::string &str)
+{
+    std::int32_t value;
+    // Check for hexadecimal format
+    if (str.rfind("0x", 0) == 0)
+    {
+        value = std::stoi(str, nullptr, 16); // Convert from hex
+    }
+    else
+    {
+        value = std::stoi(str, nullptr, 10); // Convert from decimal
+    }
+    return value;
 }
--- src/Helpers.hpp
+++ src/Helpers.hpp
@@ -10,4 +10,10 @@ std::vector<std::string> split(const std::string &s, char delim);
 
 std::string toBinaryString(uint32_t value);
 
+bool fitsIn16Bits(std::int32_t value);
+
+bool fitsIn32Bits(std::int32_t value);
+
+std::int32_t handleValue(const std::string &str);
+
 #endif
