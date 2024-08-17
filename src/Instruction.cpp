3a4b9a048663b398667c0de73d63c7d9e7957187
sebastianx9407x <sebastianx9407x@gmail.com>
2024-08-16 23:27:21 -0400

    updating handling parsing

--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -1,39 +1,73 @@
 #include "Instruction.hpp"
 #include "MIPS.hpp"
 #include "Helpers.hpp"
-#include <unordered_set>
-#include <vector>
-#include <string>
 #include <stdexcept>
 
-// Definitions of the static const sets
-const std::unordered_set<std::string> Instruction::LAYOUT_DST = {
-    "add", "addu", "sub", "subu",
-    "and", "or", "xor", "nor",
-    "slt", "sltu"};
+const std::unordered_map<std::string, int> Instruction::LAYOUT_INPUT_SIZES = {
+    {"DST", 4},      //  $d, $s, $t
+    {"ST", 3},       // mult $s, $t
+    {"S", 2},        // jr $s
+    {"DTIMM", 4},    // sll $d, $t, shamt
+    {"TSIMM", 4},    // addi $t, $s, imm
+    {"TIMM", 3},     // lui $t, imm
+    {"STOFF", 4},    // beq $s, $t, offset
+    {"TOFFS", 3},    // lw $t, offset($s)
+    {"SOFF", 3},     // bgtz $s, offset
+    {"TARG", 2},     // j target
+    {"SYSCALL", 1}}; // syscall
 
-const std::unordered_set<std::string> Instruction::LAYOUT_ST = {
-    "mult", "multu", "div", "divu"};
+const std::unordered_map<std::string, std::function<void(Instruction &, std::vector<std::string> &)>> Instruction::MNEMONIC_FUNCTION_MAP = {
+    {"add", parseDST},  // add $d, $s, $t
+    {"addu", parseDST}, // addu $d, $s, $t
+    {"sub", parseDST},  // sub $d, $s, $t
+    {"subu", parseDST}, // subu $d, $s, $t
+    {"and", parseDST},  // and $d, $s, $t
+    {"or", parseDST},   // or $d, $s, $t
+    {"xor", parseDST},  // xor $d, $s, $t
+    {"nor", parseDST},  // nor $d, $s, $t
+    {"slt", parseDST},  // slt $d, $s, $t
+    {"sltu", parseDST}, // sltu $d, $s, $t
 
-const std::unordered_set<std::string> Instruction::LAYOUT_D = {
-    "jr"};
+    {"mult", parseST},  // mult $s, $t
+    {"multu", parseST}, // multu $s, $t
+    {"div", parseST},   // div $s, $t
+    {"divu", parseST},  // divu $s, $t
 
-const std::unordered_set<std::string> Instruction::LAYOUT_S = {
-    "sll", "srl", "sra"};
+    {"jr", parseS}, // jr $s
 
-const std::unordered_set<std::string> Instruction::LAYOUT_TSIMM = {
-    "addi", "addiu", "andi", "ori",
-    "xori", "slti", "sltiu"};
+    {"sll", parseDTIMM}, // sll $d, $t, shamt
+    {"srl", parseDTIMM}, // srl $d, $t, shamt
+    {"sra", parseDTIMM}, // sra $d, $t, shamt
 
-const std::unordered_set<std::string> Instruction::LAYOUT_TOFFS = {
-    "lw", "sw", "lb", "sb",
-    "lbu", "lh", "sh"};
+    {"addi", parseTSIMM},  // addi $t, $s, imm
+    {"addiu", parseTSIMM}, // addiu $t, $s, imm
+    {"andi", parseTSIMM},  // andi $t, $s, imm
+    {"ori", parseTSIMM},   // ori $t, $s, imm
+    {"xori", parseTSIMM},  // xori $t, $s, imm
+    {"slti", parseTSIMM},  // slti $t, $s, imm
+    {"sltiu", parseTSIMM}, // sltiu $t, $s, imm
 
-const std::unordered_set<std::string> Instruction::LAYOUT_TIMM = {
-    "lui"};
+    {"lui", parseTIMM}, // lui $t, imm
 
-const std::unordered_set<std::string> Instruction::SYSCALL = {
-    "syscall"};
+    {"beq", parseSTOFF}, // beq $s, $t, offset
+    {"bne", parseSTOFF}, // bne $s, $t, offset
+
+    {"lw", parseTOFFS},  // lw $t, offset($s)
+    {"sw", parseTOFFS},  // sw $t, offset($s)
+    {"lb", parseTOFFS},  // lb $t, offset($s)
+    {"sb", parseTOFFS},  // sb $t, offset($s)
+    {"lbu", parseTOFFS}, // lbu $t, offset($s)
+    {"lh", parseTOFFS},  // lh $t, offset($s)
+    {"sh", parseTOFFS},  // sh $t, offset($s)
+
+    {"bgtz", parseSOFF}, // bgtz $s, offset
+    {"bltz", parseSOFF}, // bltz $s, offset
+
+    {"j", parseTARG},   // j target
+    {"jal", parseTARG}, // jal target
+
+    {"syscall", parseSyscall} // syscall
+};
 
 Instruction::Instruction()
 {
@@ -42,8 +76,8 @@ Instruction::Instruction()
 Instruction::Instruction(const std::string &curInstruction)
     : ASMInstruction(curInstruction)
 {
-    std::vector instructionTokens = split(curInstruction, ' ');
-    std::string mnemonic = instructionTokens[0];
+    std::vector tokens = split(curInstruction, ' ');
+    std::string mnemonic = tokens[0];
     if (MIPS::INSTRUCTIONMAP.find(mnemonic) == MIPS::INSTRUCTIONMAP.end())
     {
         throw std::runtime_error("Mnemonic not found: " + mnemonic);
@@ -53,45 +87,13 @@ Instruction::Instruction(const std::string &curInstruction)
     this->funct = MIPS::INSTRUCTIONMAP.at(this->mnemonic).funct;
 
     // Adding registers
-    if (isInLayout(Instruction::LAYOUT_DST))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_ST))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_D))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_S))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_TSIMM))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_TOFFS))
-    {
-    }
-    else if (isInLayout(Instruction::LAYOUT_TIMM))
-    {
-    }
-    else if (isInLayout(Instruction::SYSCALL))
-    {
-    }
-    else
-    {
-        std::cout << "No set for this mnemonic" << std::endl;
-    }
+    parseInstruction(*this, tokens);
     std::cout << *this << std::endl;
 }
 
 Instruction::~Instruction()
 {
     // Destructor implementation (can be empty if there's nothing to clean up)
-}
-
-bool Instruction::isInLayout(const std::unordered_set<std::string> layout)
-{
-    return layout.find(this->mnemonic) != layout.end();
 }
 
 // Define the operator<< function
@@ -102,4 +104,128 @@ std::ostream &operator<<(std::ostream &os, const Instruction &instruction)
        << "Opcode: " << instruction.opcode << "\n"
        << "Funct: " << (instruction.funct ? *instruction.funct : "None") << "\n";
     return os;
+}
+
+/**
+ * Validates if there are enough tokens for instruction
+ */
+void validateVectorSize(Instruction &instr, std::string layout, std::vector<std::string> &toks)
+{
+    try
+    {
+        int expectedSize = Instruction::LAYOUT_INPUT_SIZES.at(layout);
+
+        if (toks.size() != expectedSize)
+        {
+            throw std::runtime_error("Incorrect number of tokens for instruction: " + instr.ASMInstruction);
+        }
+    }
+    catch (const std::out_of_range &)
+    {
+        throw std::runtime_error("Layout not found in size mapping: " + layout);
+    }
+}
+
+/**
+ * Parses all instructions with specific layouts
+ */
+void Instruction::parseInstruction(Instruction &instr, std::vector<std::string> &toks)
+{
+    try
+    {
+        auto func = Instruction::MNEMONIC_FUNCTION_MAP.at(instr.mnemonic);
+        func(instr, toks);
+    }
+    catch (const std::out_of_range &e)
+    {
+        throw std::runtime_error("Mnemonic not mapped to a function");
+    }
+}
+
+/**
+ * Format:
+ * mnemonic $d, $s, $t
+ */
+void Instruction::parseDST(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $s, $t
+ */
+void Instruction::parseST(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $s
+ */
+void Instruction::parseS(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $t, $s imm
+ */
+void Instruction::parseDTIMM(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $t, $s imm
+ */
+void Instruction::parseTSIMM(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $t, imm
+ */
+void Instruction::parseTIMM(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $s, $t, offset
+ */
+void Instruction::parseSTOFF(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $t, offset($s)
+ */
+void Instruction::parseTOFFS(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic $s, offset
+ */
+void Instruction::parseSOFF(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * mnemonic target
+ */
+void Instruction::parseTARG(Instruction &instr, std::vector<std::string> &toks)
+{
+}
+
+/**
+ * Format:
+ * syscall
+ */
+void Instruction::parseSyscall(Instruction &instr, std::vector<std::string> &toks)
+{
 }
--- src/Instruction.hpp
+++ src/Instruction.hpp
@@ -4,7 +4,10 @@
 #include <cstdint>
 #include <string>
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
+#include <functional>
+#include <vector>
 #include <optional>
 
 class Instruction
@@ -14,36 +17,56 @@ public:
     Instruction();
     Instruction(const std::string &curInstruction);
     ~Instruction();
-    // Layout sets
-    static const std::unordered_set<std::string> LAYOUT_DST;
-    static const std::unordered_set<std::string> LAYOUT_ST;
-    static const std::unordered_set<std::string> LAYOUT_D;
-    static const std::unordered_set<std::string> LAYOUT_S;
-    static const std::unordered_set<std::string> LAYOUT_TSIMM;
-    static const std::unordered_set<std::string> SYSCALL;
-    static const std::unordered_set<std::string> LAYOUT_TOFFS;
-    static const std::unordered_set<std::string> LAYOUT_TIMM;
+    // Mapping sizes
+    static const std::unordered_map<std::string, int> LAYOUT_INPUT_SIZES;
+    static const std::unordered_map<std::string, std::function<void(Instruction &, std::vector<std::string> &)>> MNEMONIC_FUNCTION_MAP;
     // Attributes
     std::string ASMInstruction;
     std::string mnemonic;
     std::string rdName;
     std::string rsName;
     std::string rtName;
-    int16_t Imm;
+    int16_t imm;
     uint8_t rd;
     uint8_t rs;
     uint8_t rt;
     std::string opcode;
     std::optional<std::string> funct;
-    std::string RD;
-    std::string RS;
-    std::string RT;
-    std::string IMM;
+    std::string rdBit;
+    std::string rsBit;
+    std::string rtBit;
+    std::string immBit;
+    std::string label;
     // Method
     friend std::ostream &operator<<(std::ostream &os, const Instruction &instruction);
 
 private:
-    bool isInLayout(const std::unordered_set<std::string> layout);
+    // checking if enough tokens avaibale for instructions
+    void validateVectorSize(Instruction &instr, std::string layout, std::vector<std::string> &toks);
+    // parse instructions
+    static void parseInstruction(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $d, $s, $t
+    static void parseDST(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $s $t
+    static void parseST(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $s
+    static void parseS(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $d, $t imm
+    static void parseDTIMM(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $t, $s imm
+    static void parseTSIMM(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $t, imm
+    static void parseTIMM(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $s, $t, offset
+    static void parseSTOFF(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $t, offset($s)
+    static void parseTOFFS(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic $s, offset
+    static void parseSOFF(Instruction &instr, std::vector<std::string> &toks);
+    // mnemonic target
+    static void parseTARG(Instruction &instr, std::vector<std::string> &toks);
+    // syscall
+    static void parseSyscall(Instruction &instr, std::vector<std::string> &toks);
 };
 
 #endif
