38ab8dade1ad615fceaa789cdfe2c4bf54941a75
sebastianx9407x <sebastianx9407x@gmail.com>
2024-08-18 00:46:05 -0400

    Adding globals values

---
+++ src/Globals.cpp
@@ -0,0 +1,4 @@
+#include "Globals.hpp"
+
+uint32_t PC_START = 0x00400000; // Declaration
+uint32_t DATA_START = 0x10010000;
---
+++ src/Globals.hpp
@@ -0,0 +1,7 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#include <cstdint>
+
+extern uint32_t PC_START; // Declaration
+extern uint32_t DATA_START;
+#endif
